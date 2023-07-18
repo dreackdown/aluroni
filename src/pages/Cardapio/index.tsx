974d4a27a06fb1f33efee6f73666956f0c5411bb
dreackdown <hugofaria157@live.com>
2023-07-18 00:00:54 -0300

    adicionando filtro e buscador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,10 +2,13 @@ import styles from './Cardapio.module.scss';
 import logo from '../../assets/logo.svg'
 import Buscador from "./Buscador";
 import {useState} from "react";
+import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
 
 export default function Cardapio() {
     const [busca, setBusca] = useState("");
-
+    const [filtro, setFiltro] = useState<number | null>(null);
+    const [ordenador, setOrdenador] = useState("");
     return (
         <main>
             <nav className={styles.menu}>
@@ -23,6 +26,8 @@ export default function Cardapio() {
                     setBusca={setBusca}
                 />
                 <div className={styles.cardapio__filtros}>
+                    <Filtros filtro={filtro} setFiltro={setFiltro}/>
+                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
                 </div>
             </section>
         </main>
